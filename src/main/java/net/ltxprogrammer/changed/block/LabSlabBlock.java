67fe879696480f0f7fcf96980acc63392ced7be7
bijimio <mp4@tuta.com>
2025-04-24 22:06:33 -0700

    Bijimio Experimental Fork V7.5.9 | Fixed slab drop logic.

--- src/main/java/net/ltxprogrammer/changed/block/LabSlabBlock.java
+++ src/main/java/net/ltxprogrammer/changed/block/LabSlabBlock.java
@@ -2,11 +2,10 @@ package net.ltxprogrammer.changed.block;
 
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.state.properties.SlabType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
 
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
@@ -17,7 +16,10 @@ public class LabSlabBlock extends SlabBlock {
     }
 
     @Override
-    public List<ItemStack> getDrops(BlockState blockState, LootContext.Builder builder) {
-        return new ArrayList<>(Collections.singleton(this.asItem().getDefaultInstance()));
+    public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+        List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+        if (!dropsOriginal.isEmpty())
+            return dropsOriginal;
+        return Collections.singletonList(new ItemStack(this, state.getValue(TYPE) == SlabType.DOUBLE ? 2 : 1));
     }
 }
