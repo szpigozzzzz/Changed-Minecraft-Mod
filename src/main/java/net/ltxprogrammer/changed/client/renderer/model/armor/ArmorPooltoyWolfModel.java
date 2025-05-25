4ca8329aba759a22a13161b6a482db8c69befafe
LtxPgm <ltxprogrammer@outlook.com>
2025-05-25 12:47:16 -0500

    Added specialized pooltoy wolf armor model. Fixed armor model for taurs.
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

---
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/model/armor/ArmorPooltoyWolfModel.java
@@ -0,0 +1,175 @@
+package net.ltxprogrammer.changed.client.renderer.model.armor;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.ltxprogrammer.changed.Changed;
+import net.ltxprogrammer.changed.client.CubeListBuilderExtender;
+import net.ltxprogrammer.changed.client.renderer.animate.AnimatorPresets;
+import net.ltxprogrammer.changed.client.renderer.animate.HumanoidAnimator;
+import net.ltxprogrammer.changed.entity.ChangedEntity;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.item.ItemStack;
+
+public class ArmorPooltoyWolfModel<T extends ChangedEntity> extends LatexHumanoidArmorModel<T, ArmorPooltoyWolfModel<T>> {
+    public static final ArmorModelSet<ChangedEntity, ArmorPooltoyWolfModel<ChangedEntity>> MODEL_SET =
+            ArmorModelSet.of(Changed.modResource("armor_pooltoy_wolf_male_unified"), ArmorPooltoyWolfModel::createArmorLayer, ArmorPooltoyWolfModel::new);
+
+    private final ModelPart Head;
+    private final ModelPart Torso;
+    private final ModelPart LeftLeg;
+    private final ModelPart RightLeg;
+    private final ModelPart LeftArm;
+    private final ModelPart RightArm;
+    private final HumanoidAnimator<T, ArmorPooltoyWolfModel<T>> animator;
+
+    public ArmorPooltoyWolfModel(ModelPart modelPart, ArmorModel model) {
+        super(modelPart, model);
+        this.Head = modelPart.getChild("Head");
+        this.Torso = modelPart.getChild("Torso");
+        this.LeftLeg = modelPart.getChild("LeftLeg");
+        this.RightLeg = modelPart.getChild("RightLeg");
+        this.LeftArm = modelPart.getChild("LeftArm");
+        this.RightArm = modelPart.getChild("RightArm");
+
+        var leftLowerLeg = LeftLeg.getChild("LeftLowerLeg");
+        var leftFoot = leftLowerLeg.getChild("LeftFoot");
+        var rightLowerLeg = RightLeg.getChild("RightLowerLeg");
+        var rightFoot = rightLowerLeg.getChild("RightFoot");
+
+        animator = HumanoidAnimator.of(this).hipOffset(-1.5f).torsoWidth(4.5f).torsoLength(10.0f)
+                .addPreset(AnimatorPresets.wolfLikeArmor(Head, Torso, LeftArm, RightArm,
+                        LeftLeg, leftLowerLeg, leftFoot, leftFoot.getChild("LeftPad"), RightLeg, rightLowerLeg, rightFoot, rightFoot.getChild("RightPad")));
+    }
+
+    public static LayerDefinition createArmorLayer(ArmorModel layer) {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+
+        PartDefinition RightLeg = partdefinition.addOrReplaceChild("RightLeg", CubeListBuilder.create(), PartPose.offset(-2.5F, 10.5F, 0.5F));
+
+        PartDefinition RightThigh_r1 = RightLeg.addOrReplaceChild("RightThigh_r1", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 2.0F, -2.0F, 4.0F, 5.0F, 4.0F, layer.altDeformation), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.2182F, 0.0F, 0.0F));
+
+        PartDefinition RightLowerLeg = RightLeg.addOrReplaceChild("RightLowerLeg", CubeListBuilder.create(), PartPose.offset(0.0F, 6.375F, -3.45F));
+
+        PartDefinition RightCalf_r1 = RightLowerLeg.addOrReplaceChild("RightCalf_r1", CubeListBuilder.create().texOffs(0, 19).addBox(-1.99F, -0.125F, -2.9F, 4.0F, 6.0F, 4.0F, layer.altDeformation), PartPose.offsetAndRotation(0.0F, -2.125F, 1.95F, 0.8727F, 0.0F, 0.0F));
+
+        PartDefinition RightFoot = RightLowerLeg.addOrReplaceChild("RightFoot", CubeListBuilder.create(), PartPose.offset(0.0F, 0.8F, 7.175F));
+
+        PartDefinition RightArch_r1 = RightFoot.addOrReplaceChild("RightArch_r1", CubeListBuilder.create().texOffs(2, 20).addBox(-2.0F, -8.45F, -0.725F, 4.0F, 6.0F, 3.0F, layer.altDeformation.extend(0.005F)), PartPose.offsetAndRotation(0.0F, 7.075F, -4.975F, -0.3491F, 0.0F, 0.0F));
+
+        PartDefinition RightPad = RightFoot.addOrReplaceChild("RightPad", CubeListBuilder.create().texOffs(21, 21).addBox(-2.0F, 0.0F, -2.5F, 4.0F, 2.0F, 5.0F, layer.deformation.extend(-0.25f)), PartPose.offset(0.0F, 4.325F, -4.425F));
+
+        PartDefinition LeftLeg = partdefinition.addOrReplaceChild("LeftLeg", CubeListBuilder.create(), PartPose.offset(2.5F, 10.5F, 0.5F));
+
+        PartDefinition LeftThigh_r1 = LeftLeg.addOrReplaceChild("LeftThigh_r1", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-2.0F, 2.0F, -2.0F, 4.0F, 5.0F, 4.0F, layer.altDeformation).mirror(false), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.2182F, 0.0F, 0.0F));
+
+        PartDefinition LeftLowerLeg = LeftLeg.addOrReplaceChild("LeftLowerLeg", CubeListBuilder.create(), PartPose.offset(0.0F, 6.375F, -3.45F));
+
+        PartDefinition LeftCalf_r1 = LeftLowerLeg.addOrReplaceChild("LeftCalf_r1", CubeListBuilder.create().texOffs(0, 19).mirror().addBox(-2.01F, -0.125F, -2.9F, 4.0F, 6.0F, 4.0F, layer.altDeformation).mirror(false), PartPose.offsetAndRotation(0.0F, -2.125F, 1.95F, 0.8727F, 0.0F, 0.0F));
+
+        PartDefinition LeftFoot = LeftLowerLeg.addOrReplaceChild("LeftFoot", CubeListBuilder.create(), PartPose.offset(0.0F, 0.8F, 7.175F));
+
+        PartDefinition LeftArch_r1 = LeftFoot.addOrReplaceChild("LeftArch_r1", CubeListBuilder.create().texOffs(2, 20).mirror().addBox(-2.0F, -8.45F, -0.725F, 4.0F, 6.0F, 3.0F, layer.altDeformation.extend(0.005F)).mirror(false), PartPose.offsetAndRotation(0.0F, 7.075F, -4.975F, -0.3491F, 0.0F, 0.0F));
+
+        PartDefinition LeftPad = LeftFoot.addOrReplaceChild("LeftPad", CubeListBuilder.create().texOffs(21, 21).mirror().addBox(-2.0F, 0.0F, -2.5F, 4.0F, 2.0F, 5.0F, layer.deformation.extend(-0.25f)).mirror(false), PartPose.offset(0.0F, 4.325F, -4.425F));
+
+        PartDefinition Head = partdefinition.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, layer.dualDeformation), PartPose.offset(0.0F, 1.5F, 0.0F));
+
+        var torsoCubes = ((CubeListBuilderExtender)CubeListBuilder.create().texOffs(16, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 5.0F, 4.0F, layer.dualDeformation.extend(-0.5F, 0.0F, 0.0F)));
+        torsoCubes.removeLastFaces(Direction.DOWN);
+        torsoCubes = ((CubeListBuilderExtender)torsoCubes.finish().texOffs(16, 21).addBox(-4.0F, 5.0F, -2.75F, 8.0F, 6.0F, 4.0F, layer.dualDeformation));
+        torsoCubes.removeLastFaces(Direction.DOWN);
+        if (layer == ArmorModel.CLOTHING_INNER || layer == ArmorModel.ARMOR_INNER)
+            torsoCubes.removeLastFaces(Direction.UP);
+        else
+            torsoCubes.copyLastFaceUVStart(Direction.NORTH, Direction.UP);
+        torsoCubes = ((CubeListBuilderExtender)torsoCubes.finish().texOffs(16, 21).addBox(-4.0F, 5.0F, -1.75F, 8.0F, 6.0F, 4.0F, layer.dualDeformation.extend(-0.05F)));
+        torsoCubes.removeLastFaces(Direction.DOWN);
+        if (layer == ArmorModel.CLOTHING_INNER || layer == ArmorModel.ARMOR_INNER)
+            torsoCubes.removeLastFaces(Direction.UP);
+        else
+            torsoCubes.copyLastFaceUVStart(Direction.NORTH, Direction.UP);
+        torsoCubes = ((CubeListBuilderExtender)torsoCubes.finish().texOffs(17, 27).addBox(-4.0F, 11.0F, -2.0F, 8.0F, 1.0F, 4.0F, layer.dualDeformation.extend(-0.5F, 0.0F, 0.0F)));
+        torsoCubes.removeLastFaces(Direction.UP);
+
+        PartDefinition Torso = partdefinition.addOrReplaceChild("Torso", torsoCubes.finish(), PartPose.offset(0.0F, 1.5F, 0.0F));
+
+        PartDefinition RightArm = partdefinition.addOrReplaceChild("RightArm", CubeListBuilder.create().texOffs(40, 16).addBox(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, layer.dualDeformation), PartPose.offset(-4.5F, 3.5F, 0.0F));
+
+        PartDefinition LeftArm = partdefinition.addOrReplaceChild("LeftArm", CubeListBuilder.create().texOffs(40, 16).mirror().addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, layer.dualDeformation).mirror(false), PartPose.offset(4.5F, 3.5F, 0.0F));
+
+        return LayerDefinition.create(meshdefinition, 64, 32);
+    }
+
+    @Override
+    public void prepareVisibility(EquipmentSlot armorSlot, ItemStack item) {
+        super.prepareVisibility(armorSlot, item);
+        if (armorSlot == EquipmentSlot.LEGS) {
+            prepareUnifiedLegsForArmor(item, LeftLeg, RightLeg);
+        }
+    }
+
+    @Override
+    public void unprepareVisibility(EquipmentSlot armorSlot, ItemStack item) {
+        super.unprepareVisibility(armorSlot, item);
+        if (armorSlot == EquipmentSlot.LEGS) {
+            prepareUnifiedLegsForArmor(item, LeftLeg, RightLeg);
+        }
+    }
+
+    @Override
+    public void renderForSlot(T entity, RenderLayerParent<? super T, ?> parent, ItemStack stack, EquipmentSlot slot, PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        poseStack.pushPose();
+        this.scaleForSlot(parent, slot, poseStack);
+
+        switch (slot) {
+            case HEAD -> Head.render(poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+            case CHEST -> {
+                Torso.render(poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+                LeftArm.render(poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+                RightArm.render(poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+            }
+            case LEGS -> {
+                Torso.render(poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+                LeftLeg.render(poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+                RightLeg.render(poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+            }
+            case FEET -> {
+                LeftLeg.render(poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+                RightLeg.render(poseStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+            }
+        }
+
+        poseStack.popPose();
+    }
+
+    @Override
+    public HumanoidAnimator<T, ArmorPooltoyWolfModel<T>> getAnimator(T entity) {
+        return animator;
+    }
+
+    public ModelPart getArm(HumanoidArm arm) {
+        return arm == HumanoidArm.LEFT ? this.LeftArm : this.RightArm;
+    }
+
+    public ModelPart getLeg(HumanoidArm leg) {
+        return leg == HumanoidArm.LEFT ? this.LeftLeg : this.RightLeg;
+    }
+
+    public ModelPart getHead() {
+        return this.Head;
+    }
+
+    public ModelPart getTorso() {
+        return Torso;
+    }
+}
