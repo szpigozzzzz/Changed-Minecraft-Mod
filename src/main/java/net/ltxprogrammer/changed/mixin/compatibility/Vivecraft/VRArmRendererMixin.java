b524ac43941467c71b8f8013ad7d2fc23ca32d6c
LtxPgm <ltxprogrammer@outlook.com>
2025-05-26 23:14:21 -0500

    Added alternative mixin points for vivecraft. Fixed #1298
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/mixin/compatibility/Vivecraft/VRArmRendererMixin.java
+++ src/main/java/net/ltxprogrammer/changed/mixin/compatibility/Vivecraft/VRArmRendererMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.world.entity.HumanoidArm;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -25,21 +26,46 @@ public abstract class VRArmRendererMixin extends PlayerRenderer {
         super(p_174557_, p_174558_);
     }
 
-    @Inject(method = "renderItem", at = @At("HEAD"), cancellable = true)
-    private void latexHandOverride(ControllerType side, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn,
-                                   AbstractClientPlayer playerIn, ModelPart rendererArmIn, ModelPart rendererArmwearIn, CallbackInfo callback) {
-        if (FormRenderHandler.maybeRenderHand((VRArmRenderer)(Object)this, matrixStackIn, bufferIn, combinedLightIn, playerIn, rendererArmIn, rendererArmwearIn))
-            callback.cancel();
+    @Unique
+    private boolean renderTransfurredArmPre(ControllerType side, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn,
+                                            AbstractClientPlayer playerIn, ModelPart rendererArmIn, ModelPart rendererArmwearIn) {
+        return FormRenderHandler.maybeRenderHand((VRArmRenderer)(Object)this, matrixStackIn, bufferIn, combinedLightIn, playerIn, rendererArmIn, rendererArmwearIn);
     }
 
-    @Inject(method = "renderItem", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;disableBlend()V"), cancellable = true)
-    private void renderLayers(ControllerType side, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn,
-                                   AbstractClientPlayer playerIn, ModelPart rendererArmIn, ModelPart rendererArmwearIn, CallbackInfo callback) {
+    @Unique
+    private void renderTransfurredArmPost(ControllerType side, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn,
+                                          AbstractClientPlayer playerIn, ModelPart rendererArmIn, ModelPart rendererArmwearIn) {
         for (var layer : layers) {
             if (layer instanceof FirstPersonLayer firstPersonLayer)
                 firstPersonLayer.renderFirstPersonOnArms(
                         matrixStackIn, bufferIn, combinedLightIn, playerIn, getModel().rightArm != rendererArmIn ? HumanoidArm.LEFT : HumanoidArm.RIGHT,
                         new PoseStack(), Minecraft.getInstance().getDeltaFrameTime());
         }
+    }
+
+    @Inject(method = "renderItem", at = @At("HEAD"), require = 0, cancellable = true)
+    private void renderItemOverride(ControllerType side, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn,
+                                   AbstractClientPlayer playerIn, ModelPart rendererArmIn, ModelPart rendererArmwearIn, CallbackInfo callback) {
+        if (renderTransfurredArmPre(side, matrixStackIn, bufferIn, combinedLightIn, playerIn, rendererArmIn, rendererArmwearIn))
+            callback.cancel();
+    }
+
+    @Inject(method = "renderHand", at = @At("HEAD"), require = 0, cancellable = true)
+    private void renderHandOverride(ControllerType side, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn,
+                                   AbstractClientPlayer playerIn, ModelPart rendererArmIn, ModelPart rendererArmwearIn, CallbackInfo callback) {
+        if (renderTransfurredArmPre(side, matrixStackIn, bufferIn, combinedLightIn, playerIn, rendererArmIn, rendererArmwearIn))
+            callback.cancel();
+    }
+
+    @Inject(method = "renderItem", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;disableBlend()V"), require = 0, cancellable = true)
+    private void renderItemLayers(ControllerType side, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn,
+                                   AbstractClientPlayer playerIn, ModelPart rendererArmIn, ModelPart rendererArmwearIn, CallbackInfo callback) {
+        renderTransfurredArmPost(side, matrixStackIn, bufferIn, combinedLightIn, playerIn, rendererArmIn, rendererArmwearIn);
+    }
+
+    @Inject(method = "renderHand", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;disableBlend()V"), require = 0, cancellable = true)
+    private void renderHandLayers(ControllerType side, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn,
+                                   AbstractClientPlayer playerIn, ModelPart rendererArmIn, ModelPart rendererArmwearIn, CallbackInfo callback) {
+        renderTransfurredArmPost(side, matrixStackIn, bufferIn, combinedLightIn, playerIn, rendererArmIn, rendererArmwearIn);
     }
 }
