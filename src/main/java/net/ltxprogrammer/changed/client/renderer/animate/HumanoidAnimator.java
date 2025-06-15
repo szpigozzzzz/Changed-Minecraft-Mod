1b16bddfcc5ff7dd5511ae47b9fd90d374d6a2a6
LtxPgm <ltxprogrammer@outlook.com>
2025-06-15 12:56:58 -0500

    Adjusted AnimateStage.SLEEP to only run when the entity is in the Sleep pose, and not when isSleeping().
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/client/renderer/animate/HumanoidAnimator.java
+++ src/main/java/net/ltxprogrammer/changed/client/renderer/animate/HumanoidAnimator.java
@@ -13,10 +13,7 @@ import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.HumanoidArm;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
@@ -300,7 +297,7 @@ public class HumanoidAnimator<T extends ChangedEntity, M extends AdvancedHumanoi
     public static enum AnimateStage implements BiPredicate<HumanoidAnimator<?,?>, ChangedEntity> {
         INIT,
         RIDE((animator, latex) -> animator.entityModel.riding),
-        SLEEP((animator, latex) -> latex.isSleeping()),
+        SLEEP((animator, latex) -> latex.getPose() == Pose.SLEEPING),
         ATTACK,
         CROUCH((animator, latex) -> animator.crouching),
         STAND((animator, latex) -> !animator.crouching),
