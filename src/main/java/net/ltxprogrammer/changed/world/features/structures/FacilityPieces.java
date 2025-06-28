2a8abdc03be04bd2d624ffa0cb9b1afe71046004
Yird <100542617+Y1rd@users.noreply.github.com>
2025-06-28 14:55:01 -0700

    greenhouse hallway first test

--- src/main/java/net/ltxprogrammer/changed/world/features/structures/FacilityPieces.java
+++ src/main/java/net/ltxprogrammer/changed/world/features/structures/FacilityPieces.java
@@ -119,8 +119,9 @@ public class FacilityPieces { // TODO extend facility pieces to be data-oriented
                 .register(new FacilityCorridorSection(Changed.modResource("facility/corridor/stair/red/stairs2_red")))
 
                 // Misc
-                .register(new FacilityCorridorSection(Changed.modResource("facility/corridor/special/laser_hall")))
-                .register(FacilityPieceCollectionBuilder.WEIGHT_UNCOMMON, new FacilityCorridorSection(Changed.modResource("facility/corridor/special/bathroom_gray")));
+            .register(new FacilityCorridorSection(Changed.modResource("facility/corridor/special/laser_hall")))
+                .register(FacilityPieceCollectionBuilder.WEIGHT_UNCOMMON, new FacilityCorridorSection(Changed.modResource("facility/corridor/special/bathroom_gray")))
+                .register(FacilityPieceCollectionBuilder.WEIGHT_UNCOMMON, new FacilityCorridorSection(Changed.modResource("facility/corridor/special/gh_hallway_gray")));
     }
 
     private static void registerTransitions(FacilityPieceCollectionBuilder builder) {
