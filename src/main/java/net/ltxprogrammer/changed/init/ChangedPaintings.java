151b9f01752a2b1d198e99ba02a54fb9cd023d4e
bijimio <mp4@tuta.com>
2025-06-29 02:20:22 +0200

    Bijimio Experimental Fork V7.7.1 | Added some posters from the game, tweaked exoskeleton item display.

--- src/main/java/net/ltxprogrammer/changed/init/ChangedPaintings.java
+++ src/main/java/net/ltxprogrammer/changed/init/ChangedPaintings.java
@@ -9,11 +9,16 @@ import net.minecraftforge.fml.common.Mod;
 public class ChangedPaintings {
     @SubscribeEvent
     public static void registerMotives(RegistryEvent.Register<Motive> event) {
-        event.getRegistry().register(new Motive(16, 16).setRegistryName("dark_latex_mask"));
-        event.getRegistry().register(new Motive(32, 32).setRegistryName("puro_place"));
-        event.getRegistry().register(new Motive(64, 64).setRegistryName("sharks_gaze"));
-        event.getRegistry().register(new Motive(32, 32).setRegistryName("dr_k"));
-        event.getRegistry().register(new Motive(32, 32).setRegistryName("a_lazy_fox_on_the_paper"));
+        event.getRegistry().register(new Motive(48, 48).setRegistryName("a_lazy_fox_on_the_paper"));
         event.getRegistry().register(new Motive(64, 32).setRegistryName("creation_of_lin"));
+        event.getRegistry().register(new Motive(16, 16).setRegistryName("dark_latex_mask"));
+        event.getRegistry().register(new Motive(32, 32).setRegistryName("dr_k"));
+        event.getRegistry().register(new Motive(48, 32).setRegistryName("earth_and_moon"));
+        event.getRegistry().register(new Motive(32, 32).setRegistryName("kade_tail"));
+        event.getRegistry().register(new Motive(32, 32).setRegistryName("puro_place"));
+        event.getRegistry().register(new Motive(48, 32).setRegistryName("puropoint"));
+        event.getRegistry().register(new Motive(64, 64).setRegistryName("sharks_gaze"));
+        event.getRegistry().register(new Motive(64, 48).setRegistryName("thunder_science_building"));
+
     }
 }
